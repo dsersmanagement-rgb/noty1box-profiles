70a21a1cfb387a8680305139828a2002975ce486
dsersmanagement-rgb <dsersmanagement@gmail.com>
2025-10-28 20:31:31 +0100

    Add Google Sheets API configuration

---
+++ config.js
@@ -0,0 +1,4 @@
+// Google Sheets API Config
+const SHEET_ID = "18Hul4Us2rd_1jXRhYAHA0KOK2zuWlbFxb0A4-mJHmXg"; // Your sheet ID
+const API_KEY = "AIzaSyBXFpv32tqr_c4PK8WcSzs9stLkFxenkOM"; // Your API key
+const SHEET_NAME = "Sheet1"; // Change if your tab name is different
